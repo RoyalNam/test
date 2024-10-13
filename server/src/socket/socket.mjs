8df6f5380107a145c8e92a6a30c2a903876d41f5
RoyalNam <namhoangprovip@gmail.com>
2024-10-13 15:02:17 +0700

    updated

--- server/src/socket/socket.mjs
+++ server/src/socket/socket.mjs
@@ -10,23 +10,24 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: process.env.CLIENT_URL,
+        origin: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
     },
 });
 
+const userSocketMap = {};
+
 export const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId];
 };
 
-const userSocketMap = {}; // {userId: socketId}
-
 io.on('connection', async (socket) => {
-    console.log('a user connected', socket.id);
+    console.log('A user connected', socket.id);
 
     const userId = socket.handshake.query.userId;
-    if (userId !== 'undefined') {
+
+    if (userId && userId !== 'undefined') {
         userSocketMap[userId] = socket.id;
         await UserActivity.findOneAndUpdate({ user_id: userId }, { last_active: new Date() }, { upsert: true });
     }
@@ -34,18 +35,24 @@ io.on('connection', async (socket) => {
     io.emit('getOnlineUsers', Object.keys(userSocketMap));
 
     socket.on('disconnect', async () => {
-        console.log('user disconnected', socket.id);
-        delete userSocketMap[userId];
-        io.emit('getOnlineUsers', Object.keys(userSocketMap));
-        await UserActivity.findOneAndUpdate({ user_id: userId }, { last_active: new Date() }, { upsert: true });
+        console.log('User disconnected', socket.id);
+
+        if (userId) {
+            delete userSocketMap[userId];
+            io.emit('getOnlineUsers', Object.keys(userSocketMap));
+            await UserActivity.findOneAndUpdate({ user_id: userId }, { last_active: new Date() }, { upsert: true });
+        }
     });
 });
 
 export const sendSocketNotification = (receiverId, event, data) => {
-    if (!receiverId) {
-        console.log('Invalid receiver ID: undefined');
+    const socketId = getReceiverSocketId(receiverId);
+    if (!socketId) {
+        console.log(`Socket ID not found for receiver ID: ${receiverId}`);
         return;
     }
+
+    io.to(socketId).emit(event, data);
 };
 
 export { app, io, server };

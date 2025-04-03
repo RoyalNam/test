171addf1c02413b4c623fecaaead8ed6f4fa392d
RoyalNam <namroyal.dev@gmail.com>
2025-04-03 17:02:30 +0700

    second

--- src/hooks/useBitcoinData.ts
+++ src/hooks/useBitcoinData.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { GetCandles } from "@/services/api-binance";
 import { Timeframe } from "@/types";
 
